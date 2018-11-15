9f99c2cc51e1095c22ef73ad33100692777c8e9e
hyheleh <hyheleh@gmail.com>
2018-11-15 10:33:08 -0500

    fix student.java

--- src/neu/edu/csye6200/team/objects/Student.java
+++ src/neu/edu/csye6200/team/objects/Student.java
@@ -61,7 +61,7 @@ public class Student extends Person implements Comparable<Student>{
 	}
 	@Override
 	public String toString() {
-		return stuId + "," + PropertiesReader.getSimpleDataFormat().format(registerTime) + "," + firstName + "," + lastName + "," + age + "," + fatherName + "," + motherName + "\n";
+		return stuId + "," + firstName + "," + lastName + "," + PropertiesReader.getSimpleDataFormat().format(registerTime) + "," + age + "," + fatherName + "," + motherName + "\n";
 	}
 
 	@Override
